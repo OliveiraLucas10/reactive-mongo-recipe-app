0981f84c8d9d08654866f942d79ad9206e1751d1
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-02 16:07:06 +0100

    added web flux

--- src/main/java/com/oliveiralucaspro/controllers/ImageController.java
+++ src/main/java/com/oliveiralucaspro/controllers/ImageController.java
@@ -1,12 +1,5 @@
 package com.oliveiralucaspro.controllers;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.oliveiralucaspro.commands.RecipeCommand;
 import com.oliveiralucaspro.services.ImageService;
 import com.oliveiralucaspro.services.RecipeService;
 
@@ -45,21 +37,21 @@ public class ImageController {
 	return "redirect:/recipe/" + id + "/show";
     }
 
-    @GetMapping("recipe/{id}/recipeimage")
-    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws IOException {
-	RecipeCommand recipeCommand = recipeService.findCommandById(id).block();
-
-	if (recipeCommand.getImage() != null) {
-	    byte[] byteArray = new byte[recipeCommand.getImage().length];
-	    int i = 0;
-
-	    for (Byte wrappedByte : recipeCommand.getImage()) {
-		byteArray[i++] = wrappedByte; // auto unboxing
-	    }
-
-	    response.setContentType("image/jpeg");
-	    InputStream is = new ByteArrayInputStream(byteArray);
-	    IOUtils.copy(is, response.getOutputStream());
-	}
-    }
+//    @GetMapping("recipe/{id}/recipeimage")
+//    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws IOException {
+//	RecipeCommand recipeCommand = recipeService.findCommandById(id).block();
+//
+//	if (recipeCommand.getImage() != null) {
+//	    byte[] byteArray = new byte[recipeCommand.getImage().length];
+//	    int i = 0;
+//
+//	    for (Byte wrappedByte : recipeCommand.getImage()) {
+//		byteArray[i++] = wrappedByte; // auto unboxing
+//	    }
+//
+//	    response.setContentType("image/jpeg");
+//	    InputStream is = new ByteArrayInputStream(byteArray);
+//	    IOUtils.copy(is, response.getOutputStream());
+//	}
+//    }
 }
--- src/main/java/com/oliveiralucaspro/controllers/RecipeController.java
+++ src/main/java/com/oliveiralucaspro/controllers/RecipeController.java
@@ -2,20 +2,15 @@ package com.oliveiralucaspro.controllers;
 
 import javax.validation.Valid;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.oliveiralucaspro.commands.RecipeCommand;
-import com.oliveiralucaspro.exceptions.NotFoundException;
 import com.oliveiralucaspro.services.RecipeService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -78,19 +73,19 @@ public class RecipeController {
 	return "redirect:/";
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception) {
-
-	log.error("Handling not found exception");
-	log.error(exception.getMessage());
-
-	ModelAndView modelAndView = new ModelAndView();
-
-	modelAndView.setViewName("404error");
-	modelAndView.addObject("exception", exception);
-
-	return modelAndView;
-    }
+//    @ResponseStatus(HttpStatus.NOT_FOUND)
+//    @ExceptionHandler(NotFoundException.class)
+//    public ModelAndView handleNotFound(Exception exception) {
+//
+//	log.error("Handling not found exception");
+//	log.error(exception.getMessage());
+//
+//	ModelAndView modelAndView = new ModelAndView();
+//
+//	modelAndView.setViewName("404error");
+//	modelAndView.addObject("exception", exception);
+//
+//	return modelAndView;
+//    }
 
 }
